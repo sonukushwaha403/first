981653fd85ffad4433dc9c5bbf1d33734cea0369
sonu kumar kushwaha <sksonukushwaha403@gmail.com>
2020-07-22 00:41:33 +0530

    added if else in order section

--- src/main/java/com/example/foodorder/MainActivity.java
+++ src/main/java/com/example/foodorder/MainActivity.java
@@ -20,8 +20,24 @@ public class MainActivity extends AppCompatActivity {
         EditText editText1 = findViewById(R.id.editTextTextPersonName1);
         EditText editText2 = findViewById(R.id.editTextTextPersonName2);
         EditText editText3 = findViewById(R.id.editTextTextPersonName3);
-        String message = "order for "+editText1.getText().toString() +" , "
-                + editText2.getText().toString()+" & "+ editText3.getText().toString()+" has been succesfully placed";
+        String message;
+        if(editText1.getText().toString().isEmpty() && editText2.getText().toString().isEmpty()&&editText3.getText().toString().isEmpty()){
+            message="''SORRY'' You have not entered any food item ";
+        }else if (editText1.getText().toString().isEmpty()){
+            message = "order for " +
+                     editText2.getText().toString() + " & " + editText3.getText().toString() + " has been succesfully placed";
+        }
+        else if(editText2.getText().toString().isEmpty()){
+            message = "order for " + editText1.getText().toString()  + " & " + editText3.getText().toString() + " has been succesfully placed";
+        }
+        else if(editText3.getText().toString().isEmpty()){
+            message = "order for " + editText1.getText().toString() + " & "
+                    + editText2.getText().toString()   + " has been succesfully placed";
+        }
+       else {
+            message = "order for " + editText1.getText().toString() + " , "
+                    + editText2.getText().toString() + " & " + editText3.getText().toString() + " has been succesfully placed";
+        }
         intent.putExtra(MSG,message);
         startActivity(intent);
 
